d4e42a5c5bf47efd094e31ffbc09cadf8487fc97
renamed23 <savinpeoplefrombox@qq.com>
2026-05-10 14:16:41 +0800

    Fix Dll hijcking bug

--- crates/text-hook/build.rs
+++ crates/text-hook/build.rs
@@ -7,7 +7,7 @@ fn main() -> anyhow::Result<()> {
 
     // DLL劫持的时候，我们需要按照劫持的DLL的序号进行导出
     // 但是目前Rust并不支持在代码中指定导出序号，所以我们需要def表
-    if std::env::var("CARGO_FEATURE_DLL_HIJACKING").is_ok() {
+    if std::env::var("CARGO_FEATURE_ENABLE_DLL_HIJACKING").is_ok() {
         let manifest_dir = PathBuf::from(std::env::var("CARGO_MANIFEST_DIR")?);
         let def_path = manifest_dir.join("assets").join("temp").join("exports.def");
         println!("cargo:rerun-if-changed={}", def_path.display());
